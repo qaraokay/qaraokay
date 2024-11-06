8d422a4ab4d6ef616869838e2b786c6e9f57d7b1
qaraokay@genpi.ltd <qaraokay@genpi.ltd>
2024-11-06 16:13:33 +0400

    stripe webhook endpoint

--- backend/api-server.js
+++ backend/api-server.js
@@ -4,12 +4,30 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+/*
 // CORS implemented so that we don't get errors when trying to access the server from a different server location
 const cors = require('cors');
 app.use(cors());
 
+const corsOptions = {
+    origin: 'localhost:3000',
+    optionsSuccessStatus: 200,
+  };
+ app.use(cors(corsOptions));
+
+*/
+
+
 const dotenv = require('dotenv');
 dotenv.config();
+
+
+// Stripe config
+// This is your test secret API key.
+const stripe = require('stripe')('sk_test_51QFrgWAnuyiQyip2dSKyOxF0At3LVFFxMudG0kFdLnjvVstcFVc63LNfb669UT6hFEVznLNdKEBhqaC04oOmMJUk00YZUNnEoS');
+app.use(express.static('public'));
+
 
 
 
@@ -22,6 +40,9 @@ const itemsPool = new Pool({
     }
 });
 module.exports = itemsPool;
+
+
+
 
 
 
@@ -147,7 +168,7 @@ app.put('/bookings', async (req, res) => {
 
 
 // ------
-// Create a Stripe Session
+// Create a Stripe Checkout Session
 // STRIPE
 // POST
 
@@ -174,6 +195,66 @@ res.redirect(303, session.url);
 
 
 
+// ------
+// Stripe Webhook endpoint (to receive post-payment etc information from Stripe)
+// STRIPE
+// POST
+
+// Replace this endpoint secret with your endpoint's unique secret
+// If you are testing with the CLI, find the secret by running 'stripe listen'
+// If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+// at https://dashboard.stripe.com/webhooks
+const endpointSecret = 'whsec_pt1T7SJ7bWTryPgVjJaXSFUWVoTgOCbr';
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  let event = request.body;
+  // Only verify the event if you have an endpoint secret defined.
+  // Otherwise use the basic event deserialized with JSON.parse
+  if (endpointSecret) {
+    // Get the signature sent by Stripe
+    const signature = request.headers['stripe-signature'];
+    try {
+      event = stripe.webhooks.constructEvent(
+        request.body,
+        signature,
+        endpointSecret
+      );
+    } catch (err) {
+      console.log(`⚠️  Webhook signature verification failed.`, err.message);
+      return response.sendStatus(400);
+    }
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = event.data.object;
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+      // Then define and call a method to handle the successful payment intent.
+      // handlePaymentIntentSucceeded(paymentIntent);
+      break;
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+      // Then define and call a method to handle the successful attachment of a PaymentMethod.
+      // handlePaymentMethodAttached(paymentMethod);
+      break;
+    default:
+      // Unexpected event type
+      console.log(`Unhandled event type ${event.type}.`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
+
+
+
+  
+  
+
+
+
 
 // =========================
 
@@ -183,7 +264,14 @@ res.redirect(303, session.url);
 
 
 // Run the API server
-const port = 4242; // Stripe example 4242, old app 3000
+/*
+// Original server
+const port = 3000; // 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+*/
+
+// Stripe example
+app.listen(4242, () => console.log('Running on port 4242'));
+
